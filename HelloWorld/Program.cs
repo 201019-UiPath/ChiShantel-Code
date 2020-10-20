8253c3fa0fc4a10c48f57b2c1f854458eba78da2
schigit <72766057+schigit@users.noreply.github.com>
2020-10-20 09:55:51 -0400

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Please enter your name");
+            name = console.ReadLine();
+            Console.WriteLine("Welcome" + name);
+        }
+    }
+}
